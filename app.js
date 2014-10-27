b6ea93bb80dc760c672e5291d0386e6933c04d64
mmlxvi <quarterly24@gmail.com>
2014-10-27 09:16:31 -0500

    positions fixed

--- app.js
+++ app.js
@@ -1,49 +1,75 @@
-$(document).ready(function(){
-   
+$(document).ready(function() {
 
+var player;
 
+//the question is: what do i put in this function? everything i need to load a
+//random video is set out below the document.ready function. how can i plug in
+//the "onPlayerReady()" function when the #channelChanger button is clicked?
+//The alert in the function immediately below is just to show that the
+//button is in fact connected and working.
 
-// Your use of the YouTube API must comply with the Terms of Service:
-// https://developers.google.com/youtube/terms
+$('#dial').click(function() {
+  alert('testing');
 
-// Helper function to display JavaScript value on HTML page.
-function showResponse(response) {
-    var responseString = JSON.stringify(response, '', 2);
-    document.getElementById('response').innerHTML += responseString;
-}
-
-// Called automatically when JavaScript client library is loaded.
-function onClientLoad() {
-    gapi.client.load('youtube', 'v3', onYouTubeApiLoad);
-}
-
-// Called automatically when YouTube API interface is loaded (see line 9).
-function onYouTubeApiLoad() {
-    // This API key is intended for use only in this lesson.
-    // See http://goo.gl/PdPA1 to get a key for your own applications.
-    gapi.client.setApiKey('AIzaSyCR5In4DZaTP6IEZQ0r1JceuvluJRzQNLE');
-
-    search();
-}
-
-function search() {
-    // Use the JavaScript client library to create a search.list() API call.
-    var request = gapi.client.youtube.search.list({
-        part: 'snippet',
-        q: 'japan',
-        safeSearch: 'moderate',
-        videoDuration: 'short',
-        
-    });
-    
-    // Send the request to the API server,
-    // and invoke onSearchRepsonse() with the response.
-    request.execute(onSearchResponse);
-}
-
-// Called automatically with the response of the YouTube API request.
-function onSearchResponse(response) {
-    showResponse(response);
-}
+})
 
 });
+
+// This code loads the IFrame Player API code asynchronously.
+  var tag = document.createElement('script');
+
+  tag.src = "https://www.youtube.com/iframe_api";
+  var firstScriptTag = document.getElementsByTagName('script')[0];
+  firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+
+
+// array of videos for playback:
+
+  var videoLibrary = ['Nw1nRv0jSkY','p24QsMMS0xs', 'xxzPk_qSjrE','o-Uv5ZNk0RE',
+  '4-Kqt5YIXTs', 'a7usMy6EYZI','WTR_rEnW9yA', '5eGqFd7L5F0','WIKqgE4BwAY',
+  'XyTmXLWCazw', 'cMqLPDDHtkc'];
+
+  var randomVid = videoLibrary[Math.floor((Math.random() * 11))];
+
+
+  // 3. This function creates an <iframe> (and YouTube player)
+  //    after the API code downloads.
+
+  function onYouTubeIframeAPIReady() {
+    player = new YT.Player('player', {
+      height: '479.7',
+      width: '780',
+      videoId: randomVid,
+      playerVars: {'autoplay':1, 'controls':0},
+      events: {
+        'onReady': onPlayerReady,
+        'onStateChange': onPlayerStateChange
+
+      }
+
+    });
+  }
+
+  // 4. The API will call this function when the video player is ready.
+  function onPlayerReady(event) {
+    event.target.playVideo();
+    player.getDuration
+  }
+
+
+
+
+//  5. The API calls this function when the player's state changes.
+  // The function indicates that when playing a video (state=1),
+  // the player should play for six seconds and then stop.
+
+   var done = false;
+  function onPlayerStateChange(event) {
+    if (event.data == YT.PlayerState.PLAYING && !done) {
+      setTimeout(stopVideo, 20000);
+      done = true;
+    }
+  }
+  function stopVideo() {
+    player.stopVideo();
+  }
